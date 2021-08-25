d6ef9e05e43f52e449773f413cd2251ab6bff72d
Graham Smith <gsmitheidw@gmail.com>
2021-08-25 12:14:02 +0100

    Better uninstall, icon -> CDN

--- tools/chocolateyuninstall.ps1
+++ tools/chocolateyuninstall.ps1
@@ -1,1 +1,11 @@
-& "$env:ProgramFiles\Intel\HAXM\uninstall.exe" /S
+try {
+    $RegPath = 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\HAXM'
+    $UninstallPath = Get-ItemProperty -Path $regpath | Select-Object -ExpandProperty 'UninstallString'
+    $SilentSwitch = '/S'
+
+    Start-Process $UninstallPath $SilentSwitch -Wait
+} 
+Catch {
+    Write-Error 'Uninstall of Intel HAXM Failed' 
+}
+
